724bcf05879b82c4b72e5616ab748bfaef434f54
Rushabh Padalia <padalia.rushabh@gmail.com>
2013-07-26 18:59:37 +0800

    design enhanced and able to retrive post from database

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@
 /*
  * GET home page.
  */
+var api = new (require('../controllers/DatabaseAPI/api.js').api);
+
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+    api.ViewAllPosts(function(err , result){
+        if(!err)
+            res.render('index', { title: 'Express' , box_obj:result });
+    })
 };
