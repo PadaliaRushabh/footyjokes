e9777fa0066a30f962676ee1beb20ad620c08b14
Rushabh Rajesh Kumar Padalia <rpadalia@localhost.localdomain>
2013-12-24 23:36:45 +0800

    session bug test-6

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var express = require('express')
   , path = require('path');
 
 var app = express();
+var MongoStore = require('connect-mongo')(express);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
